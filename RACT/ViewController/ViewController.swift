78bac60fe2b91342f45ef09b3726d552421b0f68
Archie <youkun@bilibili.com>
2022-07-25 23:46:03 +0800

    demo- signal.pipe

--- RACT/ViewController/ViewController.swift
+++ RACT/ViewController/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import ReactiveCocoa
+import ReactiveSwift
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        bindVersion1()
+        bindVersion2()
     }
+    
+    private func bindVersion1() {
+        let (signal, innerObserver) = Signal<Int, Never>.pipe()
+        let outerObserver1 = Signal<Int, Never>.Observer(value: { value in
+            print("did received value: \(value)")
+        })
+        let outerObserver2 = Signal<Int, Never>.Observer { event in
+            switch event {
+            case let .value(value):
+                print("did received value: \(value)")
+            default: break
+            }
+        }
 
-
+        signal.observe(outerObserver1)
+        signal.observe(outerObserver2)
+        
+        innerObserver.send(value: 1)
+        innerObserver.sendCompleted()
+    }
+    
+    private func bindVersion2() {
+        let (signal, innerObserver) = Signal<Int, Never>.pipe()
+        signal.observeValues { value in
+            print("observeValues did received value: \(value)")
+        }
+        signal.observeValues { value in
+            print("observeValues did received value: \(value)")
+        }
+        innerObserver.send(value: 1)
+        innerObserver.sendCompleted()
+    }
 }
 
